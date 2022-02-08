d0552a4a162bc7e9bb9c9c66425f01d76ce9faa1
Or herbin <orherbin@gmail.com>
2022-02-08 21:13:10 +0200

    minor changes to html files

--- MainScores.py
+++ MainScores.py
@@ -8,11 +8,12 @@ def score_server():
     res = 0
     score_file = open("Scores.txt", 'w+').readlines()
     if score_file is None:
-        return render_template("ScoresError.html", ERROR="Can't open file"), 400
+        return render_template("ScoreError.html", ERROR="Can't open file"), 400
     while score_file:
         res = res + int(score_file.pop())
-    return render_template("ScoresSuccess.html", SCORE=res), 200
+    return render_template("Score.html", SCORE=res), 200
 
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True, port=30000)
+
